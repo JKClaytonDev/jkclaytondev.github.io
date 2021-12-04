264efc20bfa779d9b462e9fde6f82d9806cec453
Jackson Kenneth Clayton <59662161+JKClaytonDev@users.noreply.github.com>
2021-12-04 05:35:37 -0500

    new commit

--- page_data.js
+++ page_data.js
@@ -1,5 +1,6 @@
 function start(){
 	
+selectImage2('HYPERJUSTICE','justice.jpg', 'https://store.steampowered.com/app/1831780/HYPERJUSTICE/?beta=1','HyperJustice is a fast-paced third-person shooter inspired by 1980’s cop movies and television. Sprint through neon-stained Miami as you battle the mob and try to prevent the distribution of a world-ending drug.', '12k1RdPJgk4');
 
 }
 function setText(v){
@@ -8,7 +9,14 @@ function setText(v){
 function PscrollTo(v){
 	window.scrollTo(0, v)
 }
-
+function selectImage2(k, i, l,t,y){
+	document.getElementById("photo").src = "img/Screenshots/"+i;
+	document.getElementById("desc").innerText = t;
+	document.getElementById("title").innerText = k;
+	document.getElementById("playLink").href = l;
+	document.getElementById("gd").classList.remove('nullDesc');
+	document.getElementById("gd").classList.add('gameDesc');
+}
 function selectImage(k, i, l,t,y){
 	document.getElementById("photo").src = "img/Screenshots/"+i;
 	document.getElementById("desc").innerText = t;
